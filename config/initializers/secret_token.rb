4b19b3428ac8842e0d7be154d4abc98d871b5187
sinhkej <nguyenhuysinh@luvina.net>
2013-08-23 11:05:25 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProSinhkej::Application.config.secret_key_base = 'bf4f611ff14fd676e5220ce1f3007871ffb921f2bcf0f45cc09f7f02f17e45197139b2fbe483dcc924e63cefe8dfb3fe0d546f6b52342904e27211e12f6b4bd7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProSinhkej::Application.config.session_store :cookie_store, key: '_pro_sinhkej_session'
